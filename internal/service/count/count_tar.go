6d591a62dd701de816464b5ff350393f7e18f411
namikaze-dev <usmansadik96@gmail.com>
2023-01-12 10:02:47 +0100

    feat: add service to count loc of files in a tar file

---
+++ internal/service/count/count_tar.go
@@ -0,0 +1,40 @@
+package count
+
+import (
+	"archive/tar"
+	"errors"
+	"io"
+	"os"
+	"time"
+)
+
+func Tar(path string, config *Config) (CountResult, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return CountResult{}, err
+	}
+
+	var count int64
+	trd := tar.NewReader(file)
+	startTime := time.Now()
+	for {
+		thdr, err := trd.Next()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+
+		if err != nil {
+			config.Logger.Println(err)
+			continue
+		}
+
+		c := countlines(trd)
+		config.files++
+		if config.Verbose {
+			config.Logger.Println(thdr.Name, c)
+		}
+		count += int64(c)
+	}
+
+	return CountResult{Count: count, Files: config.files, Time: time.Since(startTime)}, nil
+}
